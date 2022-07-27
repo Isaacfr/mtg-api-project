bcaa4f09f2330b60bb14d021f71ecd746c70af9b
Isaacfr <isaacl.frias@outlook.com>
2022-07-27 15:12:47 -0400

    commit-7-27-2022

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# mtg-api-project
+<h1>Project Commander Partner</h1>
+<span>*This is an ongoing project where I add ideas and functionality as I go. My current focus is the format of Commander and most of the features will be directed towards helping Commander players.*</span>
+<p>This is a Fullstack Magic: The Gathering web application project that will help users with building decks and organizing the cards they want/need for decks.</p>
+
+<h2>How It's Made:</h2>
+<p>Tech used: HTML, CSS, SCSS, JavaScript, APIs</p>
+<p>I have used HTML and JavaScript to make the application functional. I utilize the Scryfall and TCGPlayer APIs for card information such as names, rarity, prices and set names.</p>
+
+<h2>Why Make this Project?</h2>
+<p>As a new player to Magic: The Gathering, I was looking into resources into building my first deck. Unfortunately, I could not find an application or website that would tell me what cards/sets I might need to look into for a specific deck. I spent hours using spreadsheets, word documents, and notepads just to build one iteration of a deck. I thought this was time-consuming and messy, having to open several documents and looking up everything on different sites. I want this application to help ease discovery and organization of cards that players are looking for. My goal is to create a hub for players to be able to import/export all the data that they have/need into an application that can reduce the time to find cards and get players back into their next commmander game sooner.</p>
+
+<h2>Lessons Learned:</h2>
+<p>Async and await functions really help the project because of the amount of information it needs to import from two different APIs. </p>
+
+<h2>Future Optimizations</h2>
+<ul>
+  <li>Make the application responsive</li>
+  <li>Add Color Symbols from MTG</li>
+  <li>Make each element of a table collapsible through the name</li>
+  <li>Add CSS</li>
+  <li>Debug undefined responses</li>
+  <li>Add Back-End Functionality (accounts, lists, authentication)</li>
+  <li>Add more features</li>
+</ul>
--- js/main.js
+++ js/main.js
@@ -11,14 +11,17 @@ function findCard(){
 //input the item and which list to put it in
 //use async, await to load all items simultaneously
 async function getFetch(item, listRef){
+
     const url = `https://api.scryfall.com/cards/named?fuzzy=${item}`;
     let searchQueryItems = [];
 
+    if(searchQueryItems[0] === "Plains" || searchQueryItems[0] === "Mountain" || searchQueryItems[0] === "Forest" || searchQueryItems[0] === "Swamp" || searchQueryItems[0] === 'Island'){
+        return null;
+    }
     const res = await fetch(url);
     const data = await res.json();
     searchQueryItems.push(data);
     loadTable(searchQueryItems[0], listRef);
-
     // .then(res => res.json())
     // .then(data =>{
     //     searchQueryItems.push(data);
@@ -31,35 +34,136 @@ async function getFetch(item, listRef){
 }
 
 //loads table with list of items
-function loadTable(items, listRef){
+async function loadTable(items, listRef){
+    if(items.name == undefined)
+    {
+        return null;
+    }
     let tbodyRef = document.getElementById(listRef);
     let newRow = tbodyRef.insertRow();
 
+    let img = newRow.insertCell(0);
+    let newImg = document.createElement('img');
+    newImg.src = items.image_uris.small;
+    // const img_res_url = await fetch(items.image_uris.small);
+    // const img_data = await img_res_url.json();
+    // newImg.src = img_data.data;
+    // newImg.src = img_data.push(data);
+    img.appendChild(newImg);
+
+    if(items.name === "Plains" || items.name === "Mountain" || items.name === "Forest" || items.name === "Swamp" || items.name === 'Island'){
+        return null;
+    }
     //orders tables with different such as name, type,  colors, cmc, rarity and then sets
-    let name = newRow.insertCell(0);
+    let name = newRow.insertCell(1);
     name.innerHTML = items.name;
 
-    let type = newRow.insertCell(1);
+    let type = newRow.insertCell(2);
     type.innerHTML = items.type_line;
 
-    let colors = newRow.insertCell(2);
+    let colorID = "x-color";
+    if(listRef =="x-list"){
+        colorID = "x-color";
+    }
+    else if(listRef ="add-list"){
+        colorID="add-color";
+    }
+
+    let symbols = newRow.insertCell(3);
+    
     if(items.colors.length == 0)
     {
-        colors.innerHTML = 'colorless';
+        // colors.innerHTML = '{C}';
+        let newElement = document.createElement('a');
+        newElement.className = 'ms ms-c';
+        symbols.appendChild(newElement);
     }
     else
     {
-        colors.innerHTML = items.colors;
-    }
+        items.colors.map(color => {
+            let newElement = document.createElement('a');
+            if(color == 'R'){
+                newElement.className = 'ms ms-r';
+                symbols.appendChild(newElement);
+                // symbols.classList.add('ms');
+                // symbols.classList.add('ms-r');
 
-    let cmc = newRow.insertCell(3);
+            }
+            else if(color == "U"){
+                newElement.className = 'ms ms-u';
+                symbols.appendChild(newElement);
+            }
+            else if(color == "G"){
+                newElement.className = 'ms ms-g';
+                symbols.appendChild(newElement);
+            }
+            else if(color == "W"){
+                newElement.className = 'ms ms-w-original';
+                symbols.appendChild(newElement);
+            }
+            else if(color == "B"){
+                newElement.className = 'ms ms-b';
+                symbols.appendChild(newElement);
+            }
+        });
+    }
+    console.log(symbols.className);
+
+    let cmc = newRow.insertCell(4);
     cmc.innerHTML = items.cmc;
 
-    let rarity = newRow.insertCell(4);
+    let rarity = newRow.insertCell(5);
     rarity.innerHTML = items.rarity;
 
+    let desc = newRow.insertCell(6);
+    let string = items.oracle_text.split(' ');
+    // if(string.join(' ').includes('{T}')){
+    //     let fixedEl = document.createElement('a');
+    //     fixedEl.className = "ms ms-tap"
+    // desc.appendChild(fixedEl);
+    // string.splice(string.indexOf('{T}:'), 1 , ":");
+    // }
+
+    // if(string.join(' ').includes('{C}')){
+    //     for(let i = 0; i<2; i++)
+    //     {
+    //         let fixedEl = document.createElement('a');
+    //         fixedEl.className = "ms ms-c"
+    //         desc.appendChild(fixedEl);
+    //         string.push(fixedEl);
+    //     }
+    // string.slice(string.indexOf('{C}{C}.'));
+    // }
+    // let stringFixed = string.map(x =>{
+    //     if(x == '{T}:')
+    //     {
+    //         fixedEl.className = "ms ms-tap";
+    //     }
+    // }).join(' ');
+
+    // let stringFixed = string.map(x => {
+    //     if(x == '{T}:')
+    //     {
+    //         fixedEl.className = ('ms ms-tap');
+    //         desc.appendChild(fixedEl);
+    //         string.splice(string.indexOf(x), 1, fixedEl, ":");
+    //     }
+    //     // else if(x === '{C}{C}.')
+    //     // {
+    //     //     for(let i = 0; i<2; i++){
+    //     //         fixedEl.className = ('ms ms-c');
+    //     //         string.splice(string.indexOf(x), 1, fixedEl, '.');
+    //     //     }
+    //     // }
+    //     else{
+    //         return x;
+    //     }
+    // });
+    console.log(string);
+    desc.innerHTML += string.join(' ');
+
     //minimize the table for space for the amount of sets there are; changes the space box into a button
-    let sets = newRow.insertCell(5);
+    let sets = newRow.insertCell(7);
     let btn = document.createElement("button");
     btn.innerHTML = 'Sets/Prices';
     btn.className = 'collapsible';
@@ -83,33 +187,53 @@ function loadTable(items, listRef){
     setTable.appendChild(setBody);
 
     //fetches each set onto the table
-    fetch(items.prints_search_uri)
-        .then(res => res.json())
-        .then(data =>{
-            setNames.push(data.data);
-            console.log(setNames);
-            setNames[0].sort((a,b) => {
-                a = a.prices.usd;
-                b = b.prices.usd;
-                return a - b;
-            })
-            setNames[0].forEach(el =>{
-                if(el.prices.usd != null)
-                {
-                            
-                let setRow = setBody.insertRow();
-                let nameOfSet = setRow.insertCell(0);
-                let cardSetPrice = setRow.insertCell(1);
-                setBody.appendChild(setRow);
+    const newRes = await fetch(items.prints_search_uri);
+    const data = await newRes.json();
+    setNames.push(data.data);
+    setNames[0].sort((a,b) => {
+        a = a.prices.usd;
+        b = b.prices.usd;
+        return a - b;
+    })
+    setNames[0].forEach(el =>{
+        if(el.prices.usd != null)
+        {
+                    
+        let setRow = setBody.insertRow();
+        let nameOfSet = setRow.insertCell(0);
+        let cardSetPrice = setRow.insertCell(1);
+        setBody.appendChild(setRow);
 
-                nameOfSet.innerHTML = el.set_name;
-                cardSetPrice.innerHTML = el.prices.usd;
-                }
-            })
-        })
-        .catch(err =>{
-            console.log(`error ${err}`)
-        });
+        nameOfSet.innerHTML = el.set_name;
+        cardSetPrice.innerHTML = el.prices.usd;
+        }
+    })
+        // .then(res => res.json())
+        // .then(data =>{
+        //     setNames.push(data.data);
+        //     console.log(setNames);
+        //     setNames[0].sort((a,b) => {
+        //         a = a.prices.usd;
+        //         b = b.prices.usd;
+        //         return a - b;
+        //     })
+        //     setNames[0].forEach(el =>{
+        //         if(el.prices.usd != null)
+        //         {
+                            
+        //         let setRow = setBody.insertRow();
+        //         let nameOfSet = setRow.insertCell(0);
+        //         let cardSetPrice = setRow.insertCell(1);
+        //         setBody.appendChild(setRow);
+
+        //         nameOfSet.innerHTML = el.set_name;
+        //         cardSetPrice.innerHTML = el.prices.usd;
+        //         }
+        //     })
+        // })
+        // .catch(err =>{
+        //     console.log(`error ${err}`)
+        // });
 
 }
 
@@ -120,11 +244,13 @@ function loadTable(items, listRef){
 
 function compareLists(){
     const list1 = document.querySelector('#text-of-deck-1').value;
-    const updatedList1 = list1.split(/\n|\t/).filter(item => item != '\t' || item == '');
+    // const updatedList1 = list1.split(/\v|\n|\t/|'1 ').filter(item => item != '\t' || item == '' || item == "\v");
+    const updatedList1 = list1.split('\n'|'1 ').filter(x => x != "" || '1 ').map(x =>x.trim()).sort((a, b) => a - b);
+    console.log(updatedList1);
     
     const list2 = document.querySelector('#text-of-deck-2').value;
-    const updatedList2 = list2.split(/\n|\t/).filter(item => item != '\t' || item == '');
-
+    // const updatedList2 = list2.split(/\v|\n|\t/|'1 ').filter(item => item != '\t' || item == '' || item == "\v");
+    const updatedList2 = list2.split('\n'|'1 ').filter(x => x != "" || '1 ').map(x =>x.trim()).sort((a, b) => a - b);
     const x_list = updatedList1.filter(item => !updatedList2.includes(item));
     const add_list = updatedList2.filter(item => !updatedList1.includes(item));
     
@@ -154,10 +280,15 @@ function compareLists(){
 1 Mystic Forge
 1 Thran Dynamo
 */
-//enter 2 lists
-//first list - the items not in second list
-//second list + the items not in first list
-//table? for prices
+
 //what else to add in table?
 //table for information
-/**/ 
+
+/*
+Add Table Functionality to Import/Export
+    Import .txt files
+    Export .txt files + spreadsheet files
+Add Description of Cards
+Make Each Card Name a Collapsible Button
+    Collapse to Image and Card Name?
+*/
